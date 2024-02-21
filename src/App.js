702ec24603414b4f1d93cfce86d8b448ce617909
junny97 <pcohad12@gmail.com>
2024-02-21 18:59:48 +0900

    input 값에 따라 카운팅되도록 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  // Reducer함수에서 다른 상태를 참조하는것은 좋지 않다
+  // 매개변수, 리턴값만 있으면 됨
   function countReducer(prevCount, action) {
-    if (action === 'UP') {
-      return prevCount + 1;
-    } else if (action === 'DOWN') {
-      return prevCount - 1;
+    if (action.type === 'UP') {
+      return prevCount + action.number;
+    } else if (action.type === 'DOWN') {
+      return prevCount - action.number;
     } else if (action === 'RESET') return 0;
   }
   const [count, countDispatch] = useReducer(countReducer, 0);
 
   function up() {
-    countDispatch('UP');
+    countDispatch({ type: 'UP', number: number });
   }
   function down() {
-    countDispatch('DOWN');
+    countDispatch({ type: 'DOWN', number: number });
   }
   function reset() {
     countDispatch('RESET');
+  }
+  function changeNumber(event) {
+    setNumber(Number(event.target.value));
   }
 
   return (
@@ -25,7 +32,9 @@ function App() {
       <input type='button' value='-' onClick={down} />
       <input type='button' value='0' onClick={reset} />
       <input type='button' value='+' onClick={up} />
+
       <span>{count}</span>
+      <input type='text' value={number} onChange={changeNumber} />
     </>
   );
 }
